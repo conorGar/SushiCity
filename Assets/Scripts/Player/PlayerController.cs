2bb3338e1f3b535d12fc96c0084bbb8f02afdf9e
conorgar <conorgar@gmail.com>
2020-01-22 20:18:42 -0500

    Initial Commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public CharacterController controller;
+    
+
+    Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal")*movementSpeed,0,Input.GetAxis("Vertical")*movementSpeed);
+
+
+        moveDirection.y = moveDirection.y + Physics.gravity.y;
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}
